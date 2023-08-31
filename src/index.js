7b755f26663e34793a73f6124185abeda976561d
Conrado <42232126+bojorquezc@users.noreply.github.com>
2023-08-31 00:09:48 -0700

    start static html

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 console.log('This webpack stuff works!');
+
+// document.body.onload = addElement;
+
+// function addElement() {
+//     const newDiv = document.createElement("div");
+//     const newContent = document.createTextNode('This is a test');
+    
+//     newDiv.appendChild(newContent);
+
+//     const currentDiv = document.getElementById("div1");
+//     document.body.insertBefore(newDiv, currentDiv);
+// }
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,20 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Development'
+    })
+  ],
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
 };
