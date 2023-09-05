469465dbe380a622917344d9f9d7da0641803c68
Conrado <42232126+bojorquezc@users.noreply.github.com>
2023-09-05 15:23:40 -0700

    Dynamic nav bar

--- src/index.js
+++ src/index.js
@@ -3,10 +3,3 @@ import logo from './images/youkoso_logo_color.svg';
 
 import initializeWebsite from "./webpage";
 initializeWebsite();
-
-const youkosoLogo = new Image();
-youkosoLogo.src = logo;
-youkosoLogo.classList.add('nav_logo');
-
-const navBar = document.getElementById("nav_bar");
-navBar.appendChild(youkosoLogo);
